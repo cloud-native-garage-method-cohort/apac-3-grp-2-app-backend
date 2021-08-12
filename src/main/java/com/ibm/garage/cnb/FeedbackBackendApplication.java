68fffa20eb72b52f69e346692ceed67adb8625fc
Arief Hidayat <arief.hidayat@ibm.com>
2021-08-12 13:41:26 +0800

    Initial commit

---
+++ src/main/java/com/ibm/garage/cnb/FeedbackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.garage.cnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/garage/cnb/FeedbackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.garage.cnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
