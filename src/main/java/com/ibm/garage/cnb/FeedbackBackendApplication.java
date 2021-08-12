2b0f8e405b76e37e3cd16bf9736b10e70e6f42dc
Arief Hidayat <arief.hidayat@ibm.com>
2021-08-12 19:12:22 +0800

    workaround to duplicate mongo init

--- src/main/java/com/ibm/garage/cnb/FeedbackBackendApplication.java
+++ src/main/java/com/ibm/garage/cnb/FeedbackBackendApplication.java
@@ -2,9 +2,15 @@ package com.ibm.garage.cnb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class
+})
 @Configuration
 public class FeedbackBackendApplication {
 
